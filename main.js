bf799f55cd1df2a93c0db2e7660342294c260ae1
NestorAguas <fredyaguas@gmail.com>
2023-10-23 23:49:52 -0500

    try-catch

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 console.log("Estamos online")
 
 const url = "http://localhost:3000/users"
-
 async function traerDatos(){
-    const data = await fetch(url);
-    let response = await data.json();
-    console.log(response);
-    return response;
+try {
+
+    
+        const data = await fetch(url);
+        let response = await data.json();
+        console.log(response);
+        return response;
+    
+    
+} catch (error) {
+    console.log({error: error});    
+}
 }
 
 let buttonData = document.getElementById("data");
